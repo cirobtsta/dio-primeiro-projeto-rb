dc029dcd22d3f2df5313fa6960c0f62380651929
Ciro Batista <cirobtsta@gmail.com>
2024-05-25 12:18:12 -0300

    v2

--- primeiro-projeto-em-ruby.rb
+++ primeiro-projeto-em-ruby.rb
@@ -3,14 +3,20 @@
 # programa simples de acesso a leitores em uma biblioteca
 # solicitar ao usuário o nome, sobrenome e idade (em uma única frase!) 
 
-puts "Olá, seja bem vindo(a)!"
-puts "informe seu nome: "
-nome = gets.chomp
-puts "informe seu sobrenome: "
-sobre = gets.chomp
-puts "informe seu ano de nascimento: "
-ano = gets.chomp.to_i
+puts "Olá! Informe: NOME SOBRENOME ANO DE NASCIMENTO"
+puts "Obs: Inserir espaço entre as informações e depois a tecla ENTER!"
+input = gets
 
-idade = 2024 - ano
+id = input.split(" ")
 
-puts "Olá #{nome} #{sobre}, voce tem #{idade} anos"
+nom1 = id[0]
+sob1 = id[1]
+ano1 = id[2].to_i
+
+
+idade = 2024 - ano1
+puts "Olá #{nom1} #{sob1}, voce tem #{idade} anos"
+
+
+
+
